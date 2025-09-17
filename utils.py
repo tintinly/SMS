13a8dccb969f0794f0f95a8e62da20859a9449e4
tintinly <821294434@qq.com>
2025-09-18 03:34:35 +0800

    优化验证码匹配规则

--- utils.py
+++ utils.py
@@ -2,22 +2,31 @@ import re
 import pyperclip
 from win10toast import ToastNotifier
 
-def extract_first_long_number(text):
-    # 匹配长度大于等于4的数字字符串
-    pattern = r'\d{4,}'
+
+
+def extract_captcha1(text):
+    # 4位到6位的验证码 开头
+    pattern = r'(【\S*】)(\d{4,6})'
+    match = re.search(pattern, text)
+    if match:
+        return match.group(2)
+    return extract_captcha2(text)
+
+def extract_captcha2(text):
+    # 4位到6位的验证码 在“码”或“碼”字样后
+    pattern = r'(【\S*】).*[码碼]+\D*(\d{4,6})'
+    match = re.search(pattern, text)
+    if match:
+        return match.group(2)
+    return extract_captcha3(text)
+
+def extract_captcha3(text):
+    # 4位到6位的验证码 任意
+    pattern = r'\d{4,6}'
     match = re.search(pattern, text)
     if match:
         return match.group(0)
     return None
-
-def match_Captcha_number(text):
-    # 匹配app和验证码
-    pattern = r'(【\S*】)\D*(\d{4,})'
-    match = re.search(pattern, text)
-    if match:
-        return match.group(2)
-    return None
-
 
 def show_toast_notification(title, message, duration=3):
     # 创建通知对象
@@ -27,18 +36,18 @@ def show_toast_notification(title, message, duration=3):
 
 
 def copy_verification_code(text):
-    number = match_Captcha_number(text)
+    number = extract_captcha1(text)
     if number:
         # 复制到剪贴板
         pyperclip.copy(number)
         print(f"已复制到剪贴板: {number}")
         # 显示通知
-        show_toast_notification("复制成功", text)
+        show_toast_notification(f"验证码复制成功: {number}", text)
         return number
     else:
         print("未找到符合条件的数字字符串")
         # 显示通知
-        show_toast_notification("复制失败", f"请检查短信验证码")
+        show_toast_notification("验证码复制失败，请检查", text)
         return None
     
 
